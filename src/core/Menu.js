2caf098b9bb127cb2c32c46cb177cfe8ff817c9e
Eduar <educartoons@gmail.com>
2019-11-03 23:47:59 -0500

    Added to Menu - Sign In - Sign Up - items and authenticate  methods

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Link, withRouter } from "react-router-dom";
 
+import { signout, isAuthenticated } from "../auth";
+
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
     return { color: "#ff9900" };
@@ -18,6 +20,102 @@ const Menu = ({ history }) => {
             Book Index
           </Link>
         </li>
+
+        {isAuthenticated() && isAuthenticated().user.role === 0 && (
+          <>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/orders")}
+                to="/orders"
+              >
+                My Orders
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/user/cart")}
+                to="/cart"
+              >
+                My Cart
+                <sup>
+                  <small
+                    className="cart-badge"
+                    style={{
+                      borderRadius: "50%",
+                      padding: "2px 6px",
+                      backgroundColor: "black"
+                    }}
+                  ></small>
+                </sup>
+              </Link>
+            </li>
+          </>
+        )}
+
+        {isAuthenticated() && isAuthenticated().user.role === 1 && (
+          <>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/users")}
+                to="/users"
+              >
+                User Index
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/orders/all")}
+                to="/orders/all"
+              >
+                Order Index
+              </Link>
+            </li>
+          </>
+        )}
+
+        {!isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signin")}
+                to="/signin"
+              >
+                Sign in
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signup")}
+                to="/signup"
+              >
+                Sign up
+              </Link>
+            </li>
+          </>
+        )}
+
+        {isAuthenticated() && (
+          <li className="nav-item">
+            <span
+              className="nav-link"
+              style={{ cursor: "pointer", color: "#ffffff" }}
+              onClick={() =>
+                signout(() => {
+                  history.push("/");
+                })
+              }
+            >
+              Sign out
+            </span>
+          </li>
+        )}
       </ul>
     </div>
   );
