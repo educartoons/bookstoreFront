600a83991fc84721806fced289d463c25d6c748a
Eduar <educartoons@gmail.com>
2019-11-03 23:44:17 -0500

    Added Menu

---
+++ src/core/Menu.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = ({ history }) => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
+            Book Index
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default withRouter(Menu);
